1db4179720df6f4ef7536c6e6f162737147809e7
Harry Mee <harrymee94@gmail.com>
2023-05-26 13:01:03 +0100

    ix

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import dynamic from "next/dynamic";
 
-const RemoteMario = dynamic(() => import("app2/mario"), { ssr: false });
-const RemoteLuigi = dynamic(() => import("app1/luigi"), { ssr: false });
+const RemoteMario = dynamic(() => import("marioApp/mario"), { ssr: false });
+const RemoteLuigi = dynamic(() => import("luigiApp/luigi"), { ssr: false });
 
 export default function Home() {
   return (

d4679a8f22c28eddbd035f7a9fe6148582afc7f2
Harry Mee <harrymee94@gmail.com>
2023-05-26 09:21:35 +0100

    host

---
+++ next.config.js
@@ -0,0 +1,52 @@
+const { withModuleFederation } = require("@module-federation/nextjs-mf");
+
+module.exports = {
+  future: { webpack5: true },
+  images: {
+    domains: ["upload.wikimedia.org"],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/mario",
+        destination: "http://localhost:3001/mario",
+      },
+      {
+        source: "/mario/:path*",
+        destination: "http://localhost:3001/mario/:path*",
+      },
+      {
+        source: "/luigi",
+        destination: "http://localhost:3002/luigi",
+      },
+      {
+        source: "/luigi/:path*",
+        destination: "http://localhost:3002/luigi/:path*",
+      },
+    ];
+  },
+  webpack: (config, options) => {
+    const mfConf = {
+      name: "shell",
+      library: {
+        type: config.output.libraryTarget,
+        name: "shell",
+      },
+      remotes: {
+        app1: "app1",
+        app2: "app2",
+      },
+      exposes: {},
+    };
+    config.cache = false;
+    withModuleFederation(config, options, mfConf);
+
+    return config;
+  },
+
+  webpackDevMiddleware: (config) => {
+    // Perform customizations to webpack dev middleware config
+    // Important: return the modified config
+    return config;
+  },
+};
---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import dynamic from "next/dynamic";
+
+const RemoteMario = dynamic(() => import("app2/mario"), { ssr: false });
+const RemoteLuigi = dynamic(() => import("app1/luigi"), { ssr: false });
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Microfrontends Demo</title>
+        <meta
+          name="description"
+          content="Demo for Microfrontends using Module Federation"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <RemoteMario />
+        <RemoteLuigi />
+      </div>
+    </div>
+  );
+}
